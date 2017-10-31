f6d57e4584fd2ca59427e9a81156fd0b111485ee
Di Zhang <zhangdi_us@163.com>
2017-10-31 03:36:43 -0400

    Add files via upload
    problem2 upload

---
+++ problem2/problem2/HW2.h
@@ -0,0 +1,15 @@
+#pragma once
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/opencv.hpp>
+#include "utils.h"
+#include <cuda.h>
+#include <cuda_runtime.h>
+#include <string>
+extern cv::Mat imageInputRGBA;
+extern cv::Mat imageOutputRGBA;
+
+extern uchar4 *d_inputImageRGBA__;
+extern uchar4 *d_outputImageRGBA__;
+
+extern float *h_filter__;
